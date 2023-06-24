489a3f74b65c7c5fbb67795efb5f1ed64d0fc523
Nitu Dubey <108494260+Nitu170287@users.noreply.github.com>
2023-06-24 03:05:03 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 //your JS code here. If required.
+
+const quoteElement = document.querySelector('.quote-display');
+const inputElement = document.getElementById('quoteInput');
+const timerElement = document.querySelector('.timer');
+
+let startTime;
+let timerInterval;
+
+
+function fetchRandomQuote() {
+  fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+      const quote = data.content;
+      quoteElement.textContent = quote;
+    })
+    .catch(error => {
+      console.log('Error fetching quote:', error);
+    });
+}
+
+// Function to start the timer
+function startTimer() {
+  startTime = Date.now();
+  timerInterval = setInterval(updateTimer, 1000);
+}
+
+// Function to update the timer display
+function updateTimer() {
+  const elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+  timerElement.textContent = `Time: ${elapsedTime} seconds`;
+}
+
+// Function to check typing accuracy
+function checkTyping() {
+  const quote = quoteElement.textContent.trim();
+  const typedText = inputElement.value.trim();
+
+  if (typedText === quote) {
+    clearInterval(timerInterval);
+    setTimeout(fetchRandomQuote, 3000);
+  }
+}
+
+// Event listener for input changes
+inputElement.addEventListener('input', checkTyping);
+
+// Fetch a random quote when the page loads
+fetchRandomQuote();
+
+
+
+
+
